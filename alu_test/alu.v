ed018f22f79ba96800abc59ad0975b485c1f96ba
wbche <chewenbin123@gmail.com>
2024-12-13 00:03:07 +0800

    update

--- alu_test/alu.v
+++ alu_test/alu.v
@@ -1,11 +1,11 @@
 module alu(input [4:0] a, input [4:0] b, input [3:0] control, output [9:0] out);
 
-wire internal_a;
-wire internal_b;
+wire [4:0] internal_a;
+wire [4:0] internal_b;
 
 assign internal_a = control[1:0] == 2'b00 ? a : 
 		    control[1:0] == 2'b01 ? {1'b0, a[3:0]} :
-		    control[1:0] == 2'b10 ? {a[4:1], 1'b0} : a&b;
+		    control[1:0] == 2'b10 ? {a[4:1], 1'b0} : a & b;
 
 assign internal_b = b;
 
